47a10d8f712c769a93e873b0329bc7a3235ff35f
hexagonally <hexagonal.joe@gmail.com>
2024-11-20 16:19:08 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-// script.js
-import { Controller } from 'stimulus';
-
-export default class extends Controller {
-    static targets = ['title', 'content', 'item'];
-
-    // Method to toggle accordion open/close state
-    toggle(event) {
-        const title = event.currentTarget;
-        const content = title.nextElementSibling;
-
-        // Check if the item is already open or not
-        const isOpen = title.getAttribute('aria-expanded') === 'true';
-
-        // Close all items
-        this.closeAllItems();
-
-        // If the item wasn't open, open it
-        if (!isOpen) {
-            title.setAttribute('aria-expanded', 'true');
-            content.classList.add('open');
-        }
+console.clear()
+if (!CSS.supports('anchor-name: --anchor')) {
+  
+  /**
+  * Run an event listener on the list.
+  * Set the bounding properties based on closest element
+  */
+  const LIST = document.querySelector('ul')
+  LIST.dataset.enhanced = true
+  let current
+  const sync = () => {
+    if (current) {
+      const BOUNDS = current.getBoundingClientRect()
+      LIST.style.setProperty('--top', BOUNDS.top)
+      LIST.style.setProperty('--right', BOUNDS.right)
+      LIST.style.setProperty('--bottom', BOUNDS.bottom)
+      LIST.style.setProperty('--left', BOUNDS.left)
+      LIST.style.setProperty('--height', BOUNDS.height)
+      LIST.style.setProperty('--width', BOUNDS.width)
     }
-
-    // Close all accordion items
-    closeAllItems() {
-        this.itemTargets.forEach(item => {
-            const title = item.querySelector('.accordion__title');
-            const content = item.querySelector('.accordion__content');
-            title.setAttribute('aria-expanded', 'false');
-            content.classList.remove('open');
-        });
+  }
+  const UPDATE = ({ x, y }) => {
+    const ARTICLE = document.elementFromPoint(x, y).closest('li').querySelector('article')
+    if (ARTICLE !== current) {
+      current = ARTICLE  
+      // Set the bounds
+      sync()
     }
+  }
+  LIST.addEventListener('pointermove', UPDATE)
+  window.addEventListener('resize', sync)
 }
