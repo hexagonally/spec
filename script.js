7f0a7f3e73f058d460ad28a757d64419b20768af
hexagonally <hexagonal.joe@gmail.com>
2024-11-20 16:29:43 +0000

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-const update = ({ x, y }) => {
-  document.documentElement.style.setProperty('--x', x)
-  document.documentElement.style.setProperty('--y', y)
-}
-const list = document.querySelector('ol')
-list.addEventListener('pointermove', update)
